a8ef173af78af799c725705a0b7c339981ad294e
Jgffyjj <smlosafo@gmail.com>
2024-10-16 15:00:33 +0000

    removed grid from this branch

--- GhanaCrimes-app/src/components/bodyGridLG.jsx
+++ GhanaCrimes-app/src/components/bodyGridLG.jsx
@@ -1,58 +1,5 @@
 const BodyGridLG = () => {
-  return (
-    <main className="overflow-hidden md:mx-auto  hidden md:block container px-[8%]">
-      <div className=" flex justify-center space-x-8  mt-9 lg:flex-wrap ">
-        <div>
-          <div className="w-[300px] ">
-            <div className="bg-slate-500 lg:max-w-[300px] lg:h-[181px] "></div>
-            <p className="text-sm text-[#f06c00]">Business</p>
-            <p className="text-[#393939] text-[24px] hover:text-[#f06c00] font-EB font-semibold">
-              Michelin pauses some French tyre factories as demand falls
-            </p>
-          </div>
-          <div className="w-[300px] ">
-            <div className="bg-slate-500 lg:w-[300px] lg:h-[181px]"></div>
-            <p className="text-sm text-[#f06c00]">Business</p>
-            <p className="text-[#393939] text-[24px] hover:text-[#f06c00] font-EB font-semibold">
-              Michelin pauses some French tyre factories as demand falls
-            </p>
-          </div>
-        </div>
-        <div>
-          <div className="w-[616px] max-w-[300px] ">
-            <div className="bg-slate-500 lg:w-[616px] lg:h-[281px]"></div>
-            <p>Michelin pauses some French tyre factories as demand falls</p>
-          </div>
-          <hr className="mt-5" />
-          <div className="flex mt-5 max-w-[616px] gap-3">
-            <div className="bg-slate-500 lg:w-[185px] lg:h-[123px]"></div>
-            <div>
-              <p className="text-sm text-[#f06c00]">Business</p>
-              <p className="text-[#393939] text-[24px] hover:text-[#f06c00] font-EB font-semibold">
-                Michelin pauses some French tyre factories as demand falls
-              </p>
-            </div>
-          </div>
-        </div>
-        <div>
-          <div className="w-[300px] ">
-            <div className="bg-slate-500 lg:w-[300px] lg:h-[181px]"></div>
-            <p className="text-sm text-[#f06c00]">Business</p>
-            <p className="text-[#393939] text-[24px] hover:text-[#f06c00] font-EB font-semibold">
-              Michelin pauses some French tyre factories as demand falls
-            </p>
-          </div>
-          <div className="w-[300px] ">
-            <div className="bg-slate-500 lg:w-[300px] lg:h-[181px]"></div>
-            <p className="text-sm text-[#f06c00]">Business</p>
-            <p className="text-[#393939] text-[24px] hover:text-[#f06c00] font-EB font-semibold">
-              Michelin pauses some French tyre factories as demand falls
-            </p>
-          </div>
-        </div>
-      </div>
-    </main>
-  );
+  return <main className="overflow-hidden hidden md:block mx-[8%]"></main>;
 };
 
 export default BodyGridLG;
