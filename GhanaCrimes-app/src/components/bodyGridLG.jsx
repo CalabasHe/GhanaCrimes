8b8e6b9a17c3da3c3266fe0496785a74c89d75cf
Jgffyjj <smlosafo@gmail.com>
2024-10-17 04:18:56 +0000

    fixed the grid system

--- GhanaCrimes-app/src/components/bodyGridLG.jsx
+++ GhanaCrimes-app/src/components/bodyGridLG.jsx
@@ -1,48 +1,107 @@
+import { Link } from "react-router-dom";
 const BodyGridLG = () => {
   return (
-    <main className="overflow-hidden hidden md:block mx-[8%]">
-      <div className="grid md:grid-cols-2 lg:grid-cols-3">
-        <div className="w-[300px] ">
-          <div className="bg-slate-500 max-w-[300px] h-[181px] "></div>
-          <p className="text-sm text-[#f06c00]">Business</p>
-          <p className="text-[#393939] text-[24px] hover:text-[#f06c00] font-EB font-semibold">
-            Michelin pauses some French tyre factories as demand falls
-          </p>
+    <main className="overflow-x-hidden  md:block">
+      <div className="px-[4%] max-w-full mt-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-7 gap-4">
+          {/*First Column*/}
+          <div className="lg:col-span-2 md:col-span-1 space-y-4">
+            <Link className="">
+              <div className="bg-slate-500 w-full h-40 object-cover" />
+              <p className="text-sm text-[#f06c00]">Business</p>
+              <p className="text-[#393939] text-xl lg:text-2xl leading-tight  hover:text-[#f06c00] font-EB font-semibold">
+                Michelin pauses some French tyre factories as demand falls
+              </p>
+            </Link>
+            <Link className="">
+              <div className="bg-slate-500 w-full h-40 object-cover" />
+              <p className="text-sm text-[#f06c00]">Business</p>
+              <p className="text-[#393939] text-xl lg:text-2xl leading-tight  hover:text-[#f06c00] font-EB font-semibold">
+                Michelin pauses some French tyre factories as demand falls
+              </p>
+            </Link>
+          </div>
+
+          {/*Second Column*/}
+          <div className="lg:col-span-3 md:col-span-1 order-first lg:order-none">
+            <div className="h-full flex flex-col space-y-4">
+              {/*First Section*/}
+              <Link>
+                <div className="bg-slate-500 w-full h-[251px] object-cover" />
+                <p className="text-sm text-[#f06c00]">Business</p>
+                <p className="text-[#393939] text-xl lg:text-4xl leading-tight  hover:text-[#f06c00] font-EB font-semibold">
+                  Michelin pauses some French tyre factories as demand falls
+                </p>
+                <p>
+                  More than 400 white-collar workers will be laid off from
+                  Northvolt, split across the battery manufacturer's sites in
+                  Skellefteå, Stockholm and Västerås.
+                </p>
+              </Link>
+              <hr className="mt-4 mb-4 w-full" />
+              {/*Second Section*/}
+              <Link className="flex gap-3">
+                {/* Text Div - Appears first on larger screens */}
+                <div className="order-2 sm:order-1">
+                  <p className="text-sm text-[#f06c00]">Business</p>
+                  <p className="text-[#393939] text-xl lg:text-2xl leading-tight hover:text-[#f06c00] font-EB font-semibold">
+                    Michelin pauses some French tyre factories as demand falls
+                  </p>
+                </div>
+
+                {/* Image Div - Appears first on mobile (default order for mobile) */}
+                <div className="bg-slate-500 w-[185px] h-[123px] object-cover flex-shrink-0 order-1 sm:order-2" />
+              </Link>
+            </div>
+          </div>
+
+          {/*Third Column*/}
+          <div className="space-y-8 lg:col-span-2 md:col-span-2">
+            <Link className="">
+              <div className="bg-slate-500 w-full h-40 object-cover " />
+              <p className="text-sm text-[#f06c00]">Business</p>
+              <p className="text-[#393939] text-xl lg:text-2xl leading-tight hover:text-[#f06c00] font-EB font-semibold">
+                Michelin pauses some French tyre factories as demand falls
+              </p>
+            </Link>
+            <Link className="">
+              <div className="bg-slate-500 w-full h-40 object-cover" />
+              <p className="text-sm text-[#f06c00]">Business</p>
+              <p className="text-[#393939] text-xl lg:text-2xl leading-tight hover:text-[#f06c00] font-EB font-semibold">
+                Michelin pauses some French tyre factories as demand falls
+              </p>
+            </Link>
+          </div>
         </div>
-        <div className="w-[300px] ">
-          <div className="bg-slate-500 max-w-[300px] h-[181px] "></div>
-          <p className="text-sm text-[#f06c00]">Business</p>
-          <p className="text-[#393939] text-[24px] hover:text-[#f06c00] font-EB font-semibold">
-            Michelin pauses some French tyre factories as demand falls
-          </p>
-        </div>
-        <div className="w-[300px] ">
-          <div className="bg-slate-500 max-w-[300px] h-[181px] "></div>
-          <p className="text-sm text-[#f06c00]">Business</p>
-          <p className="text-[#393939] text-[24px] hover:text-[#f06c00] font-EB font-semibold">
-            Michelin pauses some French tyre factories as demand falls
-          </p>
-        </div>
-        <div className="w-[300px] ">
-          <div className="bg-slate-500 max-w-[300px] h-[181px] "></div>
-          <p className="text-sm text-[#f06c00]">Business</p>
-          <p className="text-[#393939] text-[24px] hover:text-[#f06c00] font-EB font-semibold">
-            Michelin pauses some French tyre factories as demand falls
-          </p>
-        </div>
-        <div className="w-[300px] ">
-          <div className="bg-slate-500 max-w-[300px] h-[181px] "></div>
-          <p className="text-sm text-[#f06c00]">Business</p>
-          <p className="text-[#393939] text-[24px] hover:text-[#f06c00] font-EB font-semibold">
-            Michelin pauses some French tyre factories as demand falls
-          </p>
-        </div>
-        <div className="w-[300px] ">
-          <div className="bg-slate-500 max-w-[300px] h-[181px] "></div>
-          <p className="text-sm text-[#f06c00]">Business</p>
-          <p className="text-[#393939] text-[24px] hover:text-[#f06c00] font-EB font-semibold">
-            Michelin pauses some French tyre factories as demand falls
-          </p>
+        <div className="grid lg:grid-cols-4 md:grid-cols-2 gap-3 mt-11">
+          <Link>
+            <div className="bg-slate-500 w-full h-40 object-cover" />
+            <p className="text-sm text-[#f06c00]">Business</p>
+            <p className="text-[#393939] text-xl lg:text-2xl leading-tight hover:text-[#f06c00] font-EB font-semibold">
+              Michelin pauses some French tyre factories as demand falls
+            </p>
+          </Link>
+          <Link>
+            <div className="bg-slate-500 w-full h-40 object-cover" />
+            <p className="text-sm text-[#f06c00]">Business</p>
+            <p className="text-[#393939] text-xl lg:text-2xl leading-tight  hover:text-[#f06c00] font-EB font-semibold">
+              Michelin pauses some French tyre factories as demand falls
+            </p>
+          </Link>
+          <Link>
+            <div className="bg-slate-500 w-full h-40 object-cover" />
+            <p className="text-sm text-[#f06c00]">Business</p>
+            <p className="text-[#393939] text-xl lg:text-2xl leading-tight hover:text-[#f06c00] font-EB font-semibold">
+              Michelin pauses some French tyre factories as demand falls
+            </p>
+          </Link>
+          <Link>
+            <div className="bg-slate-500 w-full h-40 object-cover" />
+            <p className="text-sm text-[#f06c00]">Business</p>
+            <p className="text-[#393939] text-xl lg:text-2xl leading-tight hover:text-[#f06c00] font-EB font-semibold">
+              Michelin pauses some French tyre factories as demand falls
+            </p>
+          </Link>
         </div>
       </div>
     </main>
