8dd872631d817d379a38b372491772362ed158c3
Jgffyjj <smlosafo@gmail.com>
2024-10-18 09:53:46 +0000

    moved api from homepage.jsx to app.jsx

--- GhanaCrimes-app/src/App.jsx
+++ GhanaCrimes-app/src/App.jsx
@@ -1,7 +1,21 @@
 import { Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+
 import HomePage from "./pages/homePage";
 import NewsPage from "./pages/newsPage";
+import { getCrimes } from "../api/authAPI";
 function App() {
+  useEffect(() => {
+    const getCrimeData = async () => {
+      try {
+        const data = await getCrimes();
+        console.log(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getCrimeData();
+  }, []);
   return (
     <>
       <Routes>
--- GhanaCrimes-app/src/pages/homePage.jsx
+++ GhanaCrimes-app/src/pages/homePage.jsx
@@ -1,20 +1,7 @@
-import { useEffect } from "react";
 import Header from "../components/header";
 import MainGrid from "../components/mainGrid";
-import { getCrimes } from "../api/authAPI";
 
 const HomePage = () => {
-  useEffect(() => {
-    const getCrimeData = async () => {
-      try {
-        const data = await getCrimes();
-        console.log(data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    getCrimeData();
-  }, []);
   return (
     <div className="w-screen">
       <Header />
