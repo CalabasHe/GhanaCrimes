543e39f905eb80eb60f49533677d4ba026f4b66a
Jgffyjj <smlosafo@gmail.com>
2024-10-31 21:24:48 +0000

    ads request

---
+++ GhanaCrimes-app/src/components/adsRequestContent.jsx
@@ -0,0 +1,9 @@
+const AdsRequestContent = () => {
+  return (
+    <main className="px-3 lg:px-5 overflow-x-hidden">
+      <p>hi</p>
+    </main>
+  );
+};
+
+export default AdsRequestContent;
--- GhanaCrimes-app/src/pages/adsRequest.jsx
+++ GhanaCrimes-app/src/pages/adsRequest.jsx
@@ -1,9 +1,11 @@
 import Header from "../components/header";
 import Footer from "../components/footer";
+import AdsRequestContent from "../components/adsRequestContent";
 const AdsRequest = () => {
   return (
     <main>
       <Header />
+      <AdsRequestContent />
       <Footer />
     </main>
   );
