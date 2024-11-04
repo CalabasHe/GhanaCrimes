4630f106536266dcf057b225eb990ef1ce132829
Jgffyjj <smlosafo@gmail.com>
2024-11-04 10:24:00 +0000

    fixed search results layout

--- GhanaCrimes-app/src/components/searchResults.jsx
+++ GhanaCrimes-app/src/components/searchResults.jsx
@@ -1,6 +1,7 @@
 import { Link, useLocation } from "react-router-dom";
 import NewsCard from "./newsCard";
 import { useEffect, useState } from "react";
+import AdvertisementSection from "./adsComponents";
 
 const SearchResultsList = () => {
   const location = useLocation();
@@ -15,40 +16,30 @@ const SearchResultsList = () => {
 
   return (
     <>
-      <main className="overflow-x-hidden pt-3 px-3">
+      <main className="overflow-x-hidden pt-3 px-3 md:px-[9%]">
         {/* Topic Heading */}
-        <p className="text-xl font-bold font-EB pb-2 border-b border-black border-dotted ">
+        <p className="text-xl font-bold font-EB pb-2 border-b border-[#AEAEAE] border-dotted ">
           {" "}
           SEARCH: {query}
         </p>
-        <div className="w-full grid grid-cols-1 lg:px-[25%]  items-center pt-6 gap-y-8 gap-4">
-          {/* News Cards */}
-          {results.map((result) => (
-            <div key={result.id}>
-              <Link className="w-full flex gap-2 " to={`/news/${result.slug}`}>
-                <div className="h-[220px] w-48 lg:w-56 lg:h-48 border shadow-md">
-                  <img
-                    src={result.image?.image}
-                    alt={result.main_title}
-                    className="w-full h-full object-cover"
-                  />
-                </div>
-                <div className="pt-6">
-                  <p className="text-[0.65rem] font-medium text-[#f06c00] mt-2">
-                    {result.topic ? result.topic?.toUpperCase() : "GENERAL NEWS"}
-                  </p>
-                  <p className="text-[#393939] text-xl lg:text-2xl leading-tight hover:text-[#f06c00] font-EB font-semibold">
-                    {result.main_title.charAt(0).toUpperCase() +
-                      result.main_title.slice(1)}
-                  </p>
-                </div>
-              </Link>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-8 pt-6 gap-4">
+          {results.map((result, index) => (
+            <div
+              key={result.id}
+              className={`
+        col-span-1 
+        ${index % 3 === 0 ? "lg:col-span-2" : ""} 
+        ${index % 3 === 1 ? "lg:col-span-4" : ""} 
+        ${index % 3 === 2 ? "lg:col-span-2" : ""}
+      `}
+            >
+              <NewsCard articleData={result} />
             </div>
           ))}
         </div>
         {/* Advertisement Placeholder */}
-        <div className="h-[310px] bg-[#fafafa] mt-8 flex items-center justify-center">
-          <p className="text-[#666666]">ADVERTISEMENT</p>
+        <div className="mt-8">
+          <AdvertisementSection />
         </div>
       </main>
     </>
