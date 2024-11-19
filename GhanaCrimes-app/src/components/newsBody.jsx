25a0e31c3e7903a76ebcf6b0175ef53088cb78ae
Jgffyjj <smlosafo@gmail.com>
2024-11-19 11:02:53 +0000

    color to link

--- GhanaCrimes-app/src/components/newsBody.jsx
+++ GhanaCrimes-app/src/components/newsBody.jsx
@@ -284,7 +284,7 @@ const NewsComponent = () => {
           </p>
 
           <div
-            className="mt-5 text-[#393939] font-EB lg:text-2xl leading-loose [&_a]:text-orange-500 [&_a]:underline"
+            className="mt-5 text-[#393939] font-EB lg:text-2xl leading-loose [&_a]:text-[#f06c00]"
             dangerouslySetInnerHTML={{ __html: article?.description }}
           />
 
